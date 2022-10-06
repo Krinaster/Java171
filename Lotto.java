e49bcf3ecec735632392da90ba12b88e845af770
Krinaster <45246669+Krinaster@users.noreply.github.com>
2022-10-06 09:44:35 -0500

    Update Lotto.java

--- Lotto.java
+++ Lotto.java
@@ -1,3 +1,7 @@
+// Author		 : Caden Boldenow
+// Date Created  : 9/31/22
+// Last Modified : 10/6/22
+// Description	 : Program collects user info to generate random loterry ticket
 
 import java.util.Scanner;
 
@@ -20,7 +24,8 @@ public class Lotto {
 	char enteredKey;
 	
 	
-	System.out.println("Welcome to this random number generator for Lottery Tickets.");
+	System.out.println("Welcome to this random number generator " + 
+	"for Lottery Tickets.");
 	
 	
 	// Month calculations for Number 1 in Lottery Ticket
@@ -36,7 +41,8 @@ public class Lotto {
 		enteredDay = DEFAULT_DAY;
 	
 	// Wage
-	System.out.print("Please enter desired wage from your job or current wage: ");
+	System.out.print("Please enter desired wage from your job or" +
+	" current wage: ");
 	enteredWage = keyboard.nextDouble();
 	if(enteredWage < 0 )
 		enteredWage = DEFAULT_WAGE;
@@ -44,9 +50,11 @@ public class Lotto {
 	// used to eat leftover scanner data for next line to work
 	keyboard.nextLine();
 	// Favorite Phrase
-	System.out.print("Please enter your favorite phrase or a phrase you live by: ");
+	System.out.print("Please enter your favorite phrase or a " +
+	"phrase you live by: ");
 	enteredPhrase = keyboard.nextLine();
-	// Need to default phrase for when phrase is wrong
+	if(enteredPhrase.length() < 1)
+		enteredPhrase = DEFAULT_PHRASE;
 	
 	// Angle you like
 	System.out.print("Please enter an angle (in degrees) you like: ");
@@ -58,71 +66,34 @@ public class Lotto {
 	
 	
 	// Calculations for the inputted values, then printing lottery ticket
+	// Redefined Wage, Phrase, and Key into int after gathering them as double
+	// string, and char
 	enteredMonth = ((enteredMonth * 198) % 70) + 1;
 	enteredDay = (int)(Math.pow(enteredDay, 4) % 70 +1);
-	int Wage = (int)(enteredWage * 100) % 70 + 1;
+	int Wage = (int)(enteredWage * 100) % 70 + 1; 
 	int Phrase =(int)((Math.pow(enteredPhrase.length(), 3) % 70) + 1);
-	enteredAngle = (int)(Math.abs(Math.sin((enteredAngle * 180)/Math.PI)) * 69) + 1;
+	enteredAngle = (int)(Math.abs(Math.sin((enteredAngle * 180)/Math.PI)) * 69)
+		+ 1;
 	int Key = (int)enteredKey % 69 + 1;
 
 
 	// If else statement checking if any values are equal and giving error 
 	// if values are equal
-	if(enteredMonth == enteredDay || enteredMonth == Wage || enteredMonth == Phrase || enteredMonth == enteredAngle || enteredMonth == Key ||
-	 enteredDay == Wage || enteredDay == Phrase || enteredDay == enteredAngle || enteredDay == Key || 
-	  Wage == Phrase || Wage == enteredAngle || Wage == Key || Phrase == enteredAngle || Phrase == Key || enteredAngle == Key)
-	  System.out.println("There was an error generating your lottery ticket. Please reenter the program");
+	if(enteredMonth == enteredDay || enteredMonth == Wage || 
+		enteredMonth == Phrase || enteredMonth == enteredAngle || 
+		enteredMonth == Key || enteredDay == Wage || enteredDay == Phrase || 
+		enteredDay == enteredAngle || enteredDay == Key || 
+	    Wage == Phrase || Wage == enteredAngle || Wage == Key || 
+		Phrase == enteredAngle || Phrase == Key || enteredAngle == Key)
+			System.out.println("There was an error generating your lottery " + 
+			"ticket. Please reenter the program");
 	 
 	 else{
 		System.out.print("\nAnd your lottery ticket numbers is: "
 		+ enteredMonth + "-" + enteredDay + "-" + Wage + "-" +
 		Phrase + "-" + enteredAngle + " and the powerball is: " + Key);
 	 }
-	// Cleanup code and take out all system.out.print that show values of variable
-	// Finish documenting code
-	// Do extra credit that just checks that none of the values are the saem
-	// Make sure everything has a default value to go to 
-	// and remove imports that are not needed
 	
-	
-	
-	
-
 	}
 }
 
-
-// JTextField questionField = new JTextField(5);
-	
-	// JPanel questionPanel = new JPanel();
-	
-	// questionPanel.setLayout(new BoxLayOut(questoinPanel, BoxLayout.PAGE_AXIS));
-	
-	// questionPanel.add(new JLabel("Please enter the rquested values below"));
-	
-	// questionPanel.add(Box.createVerticalStrut(desiredvalue));
-	
-						// This is field created in JTextField
-	// questionPanel.add(questionField);
-	
-	// To get input from panel, do whateverfield.getText()
-	
-/* PROGRAM #3
-LOTTO NUMBERS: 1-69 6#'s _ _ _ _ _ _ PowerBall # _
-PowerBall# is 69/hourlywage cast as (int)(69/wage)
-First number # (favorite month x 13) % 69 + 1
-Second number # day of birth squared % 69 + 1 (Done using power function in math class)
-Third number # (HS graduation or GED year / 2) % 69 + 1
-Fourth Number # favorite phrase or movie title, take the length of the string and % 69 + 1 favMovie = "" then favMovie.length
-Fifth number# Give an angle in degrees, and calculate |sinΘ| * 68 + 1 ( Math.als(Math.sin(radian value)) and convert to radians with math.pi
-going to have to cast with (int)(calculation)
-Sixth number# key on keyboard, and (take ascii value + 3/4 * 5) % 69 + 1
-If statements are going to be used to make invalid numbers
-Invalid 
-month = DEFAULTMONTH
-if month is outside month range, set month to a default value
-do this for all that require forced number ranges
-default day
-year
-wage 
-month */
