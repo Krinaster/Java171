353bc5329483eb63f16b9059c31875dd855d785c
Krinaster <45246669+Krinaster@users.noreply.github.com>
2022-10-13 09:41:49 -0500

    Add files via upload

---
+++ Decipher.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class Decipher {
+
+	final static String PLAIN_TXT = "plain.txt";
+	final static String ENCRYPTED_TXT = "ciphertext.txt";
+	
+	public static void main(String [] args){
+		
+
+		
+		
+	}
+
+}
