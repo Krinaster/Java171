7a5d773a16db831e62fd34a64044c9878a244274
Krinaster <45246669+Krinaster@users.noreply.github.com>
2022-11-13 22:22:35 -0600

    Update Reservations.java

--- Reservations.java
+++ Reservations.java
@@ -1,13 +1,14 @@
 package reservations;
 
+import java.util.Random;
 import java.util.Scanner;
 
 
 public class Reservations {
     
     public static final int ROW_SIZE = 10;
-    public static final int LEFT_COL_SIZE = 8;
-    public static final int RIGHT_COL_SIZE = 5;
+    public static final int LEFT_COL_SIZE = 4;
+    public static final int RIGHT_COL_SIZE = 3;
     public static final char OPEN_SEAT = 'O';
     public static final char SEAT_TAKEN = 'X';
      
@@ -24,33 +25,13 @@ public class Reservations {
     // I have the +1 and the +2 respectively to make room for a row and 2 columns for the header and then the aisle
     String[][] seats = new String[ROW_SIZE+1][LEFT_COL_SIZE + RIGHT_COL_SIZE+2];
     displayMenu(seats);
-    // Initializes the entire array to open seats and then for loops after make the display menu
-    //for(int i =0; i<seats.length; i++)
-    //    for(int j=0; j<seats[i].length; j++)
-    //        seats[i][j] = "" + OPEN_SEAT;
-    
-    //For loop to make heading for rows
-    //for(int i=1; i<seats.length; i++)
-    //    seats[i][0] ="" + (char)((int)'@'+i);
-    
-    // For loop to make heading for coloumns
-    // for(int i=0; i<LEFT_COL_SIZE+1; i++)
-    //    seats[0][i] = "" + i;
-   // For loop to make the right column headers whilst skipping the aisle and numbering consecutively
-   // for(int i=LEFT_COL_SIZE+1; i<LEFT_COL_SIZE+ RIGHT_COL_SIZE + 1; i++)
-    //    seats[0][i+1] = "" + i;
-   
-    // For loop to make aisle 
-    //for(int i =0; i<seats.length; i++)
-    //    seats[i][LEFT_COL_SIZE+1] = "|";
-    
    
     } // End of main
     
     public static void print(String m[][]){
         for(int i =0; i<m.length; i++){
             for(int j=0; j<m[i].length; j++){
-                System.out.print(m[i][j] + " ");}
+                System.out.printf("%-4s", m[i][j]);}
             System.out.println("");
         }
     
@@ -59,8 +40,9 @@ public class Reservations {
     public static void displayMenu(String seats[][]){
         Scanner keyboard = new Scanner(System.in);
         String seatTry = null;
-        String[] STRING_SPLITTER = new String[(LEFT_COL_SIZE+RIGHT_COL_SIZE%10)+2];
-        
+        String[] STRING_SPLITTER = new String[2];
+        boolean exit = false, rightOfAisle = false;
+        Random rand = new Random();
         do{
             System.out.println("Welcome to our AirLine Seat Reservations");
             System.out.println();
@@ -68,7 +50,15 @@ public class Reservations {
             for (String[] seat : seats) 
                 for (int j = 0; j < seat.length; j++) 
                     seat[j] = "" + OPEN_SEAT;
-    
+            // Randomizes seat arrangement
+            for(int i =0; i<seats.length; i++)
+                for(int j=0; j<seats[i].length; j++){
+                    int k = rand.nextInt(100)+1;
+                    if(k% 2 == 0)
+                        seats[i][j] = "" + SEAT_TAKEN;
+                    else
+                        seats[i][j] = "" + OPEN_SEAT;
+                }
             //For loop to make heading for rows
             for(int i=1; i<seats.length; i++)
                 seats[i][0] ="" + (char)((int)'@'+i);
@@ -83,17 +73,21 @@ public class Reservations {
             // For loop to make aisle
             for (String[] seat : seats)
                 seat[LEFT_COL_SIZE+1] = "|";
-          
+            
+            
+            // Makes top left corner a blank
+            seats[0][0] = "";
+            
             print(seats);
             
             System.out.println();
-            // Maybe split this into two options, where you can run the program or you can quit
-            // and or randomly fill the seating chart 
+            // Should turn this into a menu prompt that then leads to a switch statement with all the options
             System.out.println("Please enter the row number and "
                     + "seat number you desire (Enter -1 to quit): ");
             // Stores whatever the person inputs as seat number
             seatTry = keyboard.next();
-            
+            if(seatTry.equals("-1"))
+                exit = true;
             // Using this method the input is not given is split into however many characters
             // need to later retry if the input is not A) two characters
             // then B) specifically what we want as the input
@@ -101,19 +95,47 @@ public class Reservations {
             // Will troubleshoot this method later for expasion
             // this method here splits bigger assignments like A13 into A 1 3
             // which is something we dont want
+            // Current method is limited to only a letter and number input
             STRING_SPLITTER = seatTry.split("");
-            for(String character : STRING_SPLITTER)
+            
+            if(isValid(STRING_SPLITTER, seats) == false)
+                System.out.println("Invalid seat, please enter a valid seat");
+            else{
+                
+            }
+                
+            // Test for seat choice
+            /*for(String character : STRING_SPLITTER)
                 System.out.print(character + " ");
             System.out.println();
-            // Now I gotta figure out how to split the string into 2 parts based on the input,
-            // and tell them to retry if a given input is not acceptable
-            // maybe can use the valid seat choice method to do that and check seat availability
-            // although that would not tell you the problem
-            
+            System.out.println(isValid(STRING_SPLITTER, seats));
+            */
         }
-    while(!seatTry.equals("-1"));
+    while(!exit == true);
     
     } // End of displayMenu method
+    // Need to figure out if else statement to add 1 to the index when past the aisle
+    // When figuring out whether the seat is takne or not
+    public static boolean isValid(String[] o, String[][] m){
+        boolean valid;
+        //Checks if the input is length two for the seat number and letter
+        if(o.length != 2)
+            valid = false;
+        //Checks if the input is equal to the first coloumn, and if so returns valse
+        else if(Integer.parseInt(o[1]) == 0)
+            valid = false;
+        // Makes sure the first value of the input is corresponding row letter
+        else if(o[0].charAt(0) < 65)
+            valid = false;
+        // Checks if the seat is taken, and if so returns false
+        if(Integer.parseInt(o[1]) < LEFT_COL_SIZE + 1)
+            valid = !m[o[0].charAt(0)-64][Integer.parseInt(o[1])].equals("" + SEAT_TAKEN);
+        else if(Integer.parseInt(o[1]) >= LEFT_COL_SIZE + 1)
+            valid = !m[o[0].charAt(0)-64][Integer.parseInt(o[1])+1].equals("" + SEAT_TAKEN);
+        else
+            valid = true;
+        return valid;
+    }
     
 } // End of class
 
