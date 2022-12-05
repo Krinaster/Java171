d3dffb5120da26df9b4d3e682ea446309a0688ff
Krinaster <45246669+Krinaster@users.noreply.github.com>
2022-12-05 00:08:52 -0600

    Update CircleClient.java

--- circleclient/CircleClient.java
+++ circleclient/CircleClient.java
@@ -1,4 +1,3 @@
-
 package circleclient;
 
 
@@ -8,7 +7,7 @@ public class CircleClient {
        
        Circle c1 = new Circle();
        Circle c2 = new Circle(4, new int[]{4,5});
-       Circle c3 = new Circle(-2, 5, 3);
+       Circle c3 = new Circle(4, 4, 5);
        System.out.println(c1.toString());
        System.out.println(c2.toString());
        System.out.println(c3.toString());
@@ -18,6 +17,14 @@ public class CircleClient {
        
        System.out.println(c2.area());
        System.out.println(c2.circumference());
+       
+       c2.shift(10,14);
+       System.out.println(c2.toString());
+       
+       System.out.println(c3.areCongruent(c2));
+       
+       // Troubleshoot equals method
+       System.out.print(c2.equals(c3));
     }
     
 }
